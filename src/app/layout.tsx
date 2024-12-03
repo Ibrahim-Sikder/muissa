86b90ecd988b566353c6cdf4b7c66614c06f50f8
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-12-03 17:18:06 +0600

    google-tag-manager-add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
 import { Hind_Siliguri } from "next/font/google";
 import "./globals.css";
@@ -9,12 +8,6 @@ import dynamic from "next/dynamic";
 import MessageIcons from "@/components/Message/MessageIcons";
 import Script from "next/script";
 
-const BackTopButton = dynamic(
-  () => import("@/components/BackTopButton/BackTopButton"),
-  {
-    ssr: false,
-  }
-);
 
 const hindiSiliguri = Hind_Siliguri({
   subsets: ["bengali"],
@@ -40,49 +33,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
-
-
-
-
-
-
-
-{/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN7C6NVT"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
-
-
-
-
   return (
     <html lang="en" data-scroll="0">
-      {/*  GTM initialization script in the */}
       <head>
+        {/* Google Tag Manager Script */}
         <Script
           id="gtm-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PN7C6NVT');
             `,
           }}
         />
-{/*         
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PN7C6NVT');</script> */}
-
       </head>
       <Providers>
         <body className={hindiSiliguri.className}>
-          {/* GTM noscript fallback */}
+          {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-PN7C6NVT"
@@ -91,6 +65,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
           </noscript>
+
           <Toaster position="bottom-right" richColors />
           <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
           <MessageIcons />
