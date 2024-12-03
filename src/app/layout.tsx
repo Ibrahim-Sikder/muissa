acac70be17415c9d489bdf3601d6ee327532c21b
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-12-03 16:57:45 +0600

    google-tag-manager-add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,15 @@ export default function RootLayout({
 
 
 
+
+
+
+{/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN7C6NVT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+
+
+
+
   return (
     <html lang="en" data-scroll="0">
       {/*  GTM initialization script in the */}
@@ -63,6 +72,13 @@ export default function RootLayout({
             `,
           }}
         />
+{/*         
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PN7C6NVT');</script> */}
+
       </head>
       <Providers>
         <body className={hindiSiliguri.className}>
